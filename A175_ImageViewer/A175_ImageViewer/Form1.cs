4e817a8a2b12acaa7593b253f6025477b8eb923a
Yoon SangHoon <54784919+KooEunBam@users.noreply.github.com>
2022-04-26 19:10:40 +0900

    Add files via upload

---
+++ A175_ImageViewer/A175_ImageViewer/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace A175_ImageViewer
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+      this.Text = "ImageViewer";
+      pictureBox1.BackColor = Color.White;
+    }
+
+    private void 이미지선택ToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      openFileDialog1.Filter = "이미지 파일(.jpg)|*.jpg|모든 파일(*.*)|*.*|실행파일(.exe)|*.exe";
+      openFileDialog1.Title = "이미지 열기";
+      openFileDialog1.FileName = "";
+      openFileDialog1.ShowDialog();
+      if (openFileDialog1.FileName != "")
+      {
+        pictureBox1.Image = Bitmap.FromFile(openFileDialog1.FileName);
+      }
+      pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+    }
+
+    private void normalToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+    }
+
+    private void stretchImageToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+    }
+
+    private void autoSizeToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+    }
+
+    private void centerImageToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+    }
+
+    private void zoomToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+    }
+
+    //protected override void OnPaint(PaintEventArgs e)
+    //{
+    //  pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+    //}
+
+    private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      Close();
+    }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
